287388b72a631d8c1ea781b754994a9ff904baf0
Itay Mishan <mishan.itay@gmail.com>
2016-06-04 18:27:10 -0400

    change model

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  comment    :string
+#  amount     :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Expense < ActiveRecord::Base
 end
